dfcdb0bccccd632e4600aae750c548682fb535e4
hyensuoh <49800920+hyensuoh@users.noreply.github.com>
2019-04-20 11:38:48 +0900

    commit for 3 files

---
+++ 03.py
@@ -0,0 +1,29 @@
+print("1 to 9")
+for i in range(1, 10, 1):
+    print(i, end=" ")
+print()
+
+print("0부터 9까지")
+for i in range(10):
+    print(i, end=" ")
+print()
+
+print("0부터 10까지")
+for i in range(11):
+    print(i, end=" ")
+print()
+
+print("1 to 10")
+for i in range(1, 11):
+    print(i, end=" ")
+print()
+
+print("1 to 10 홀수")
+for i in range(1, 11, 2):
+    print(i, end=" ")
+print()
+
+print("1 to 10 짝수")
+for i in range(2, 11, 2):
+    print(i end=" ")
+print()
